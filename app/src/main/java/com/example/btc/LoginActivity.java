e8ed35eaa3a17808835ebce1e07816d85b9d8e39
Rahim Askarzadeh <raskarzadeh1@my.bcit.ca>
2022-03-06 18:46:25 -0800

    updated login page design

--- app/src/main/java/com/example/btc/LoginActivity.java
+++ app/src/main/java/com/example/btc/LoginActivity.java
@@ -1,15 +1,30 @@
 package com.example.btc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Get input text
+        TextInputLayout textInputLayoutUsername = findViewById(R.id.TextView_login_username);
+        TextInputLayout textInputLayoutPassword = findViewById(R.id.TextView_login_password);
+
+
+
     }
+
+
+
+
 
 }
