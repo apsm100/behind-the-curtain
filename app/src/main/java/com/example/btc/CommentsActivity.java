4083713ee1c9ce7d9301cf80a04d6f40fe9528ba
apsm100 <amritmanhas11@gmail.com>
2022-03-14 20:20:41 -0700

    Adjust CommentsActivity view, hide close button rely on back button

--- app/src/main/java/com/example/btc/CommentsActivity.java
+++ app/src/main/java/com/example/btc/CommentsActivity.java
@@ -52,8 +52,8 @@ public class CommentsActivity extends FirebaseAuthentication {
     }
 
     private void initializeViews() {
-        Button closeButton = findViewById(R.id.button_comments_done);
-        closeButton.setOnClickListener(view -> finish());
+//        Button closeButton = findViewById(R.id.button_comments_done);
+//        closeButton.setOnClickListener(view -> finish());
 
         Confession model = (Confession) getIntent().getSerializableExtra("postObject");
 
@@ -123,7 +123,7 @@ public class CommentsActivity extends FirebaseAuthentication {
             textview_empty_placeholder.setVisibility(View.GONE);
         } else {
             textview_empty_placeholder.setVisibility(View.VISIBLE);
-            textview_empty_placeholder.setText("No Comments Yet...");
+            textview_empty_placeholder.setText(R.string.comments_empty_recyclerview);
         }
 
         textEditor.getEditText().addTextChangedListener(new TextWatcher() {
